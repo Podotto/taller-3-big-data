0aefb16c4087fc3d0133ee61d271ee7ad309709e
Podotto <grethelhalphen@gmail.com>
2026-03-12 18:49:18 -0500

    Creación de la lista y adición de los elementos iniciales.

---
+++ 01_shopping_list.py
@@ -0,0 +1,13 @@
+def main():
+  
+  # Lista
+  shopping_list = []
+  
+  # Agregar elementos
+  shopping_list.append("Arroz")
+  shopping_list.append("Lentejas")
+  shopping_list.append("Leche")
+  shopping_list.append("Tomate")
+  shopping_list.append("Plátano")
+  
+main()
