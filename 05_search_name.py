2da68b068bd473fcc7905ff16ec8ee833e593f6d
Podotto <grethelhalphen@gmail.com>
2026-03-13 18:53:14 -0500

    Se desarrolló la función para buscar nombres y las distintas opciones del menú.

--- 05_search_name.py
+++ 05_search_name.py
@@ -1,6 +1,36 @@
 def main():
   
-  names = ["León", "Grethel", "Maria", "Emilio", "Kamila"]
-  print(names)
+  # Lista de nombres
+  names = ["León", "Grethel", "Maria", "Emilio", "Kamila", "Mia", "Elliot"]
+  
+  # Función para buscar nombres
+  def search_name():
+    name_input = input("Write the name that you want to search: ")
+    
+    if name_input in names:
+      print("Student found.")
+    else:
+      print("Student not found.")
+      
+  # Menú
+  while True:
+    
+    print("Student Database")
+    print("1. Search name\n2. Exit")
+    
+    option = int(input("Select an option (1-2): "))
+    
+    # Casos
+    match option:
+      
+      case 1:
+        search_name()
+        
+      case 2:
+        print("Exit.")
+        break
+      
+      case 3:
+        print("Invalid option")
   
 main()
