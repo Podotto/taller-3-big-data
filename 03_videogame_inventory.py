7ab278408fc3dcd655ef39028bc7debf9a1db489
Podotto <grethelhalphen@gmail.com>
2026-03-12 19:39:02 -0500

    Función para añadir items al inventario.

---
+++ 03_videogame_inventory.py
@@ -0,0 +1,12 @@
+def main():
+  
+  #Lista para el inventario
+  inventory = []
+  
+  #Función para agregar items a la lista
+  def add_inventory():
+    add_item = input("Añade un item al inventario: ")
+    inventory.append(add_inventory)
+    return add_inventory
+  
+main()
